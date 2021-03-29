8b67063744ccc4e6cb39ad52e626c9475bca4358
vikash2806 <vikashrajendran2806@gmail.com>
2021-03-29 09:18:08 +0530

    task adding added, via eventLIstner

--- DOM-Task/app.js
+++ DOM-Task/app.js
@@ -1,8 +1,26 @@
+// Defining Var 
 const task = document.querySelector('#task');
+const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-task');
+const filter = document.querySelector('#filter');
 
-function addTask(){
+// Load all event listners
+loadEventListners();
+
+function loadEventListners(){
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e){
     let li = document.createElement("li");
-    li.value = task.value;
-    console.log(li.value);
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(task.value));
+    let link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link)
+    taskList.appendChild(li);
+    task.value = "";
+    e.preventDefault();
 }
