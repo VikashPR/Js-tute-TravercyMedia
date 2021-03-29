ac60124cf31ff107b19c0574da1bafef585b9a1c
vikash2806 <vikashrajendran2806@gmail.com>
2021-03-29 09:42:45 +0530

    alert added

--- DOM-Task/app.js
+++ DOM-Task/app.js
@@ -8,19 +8,24 @@ const filter = document.querySelector('#filter');
 // Load all event listners
 loadEventListners();
 
-function loadEventListners(){
+function loadEventListners() {
     form.addEventListener('submit', addTask);
 }
 
-function addTask(e){
-    let li = document.createElement("li");
-    li.className = 'collection-item';
-    li.appendChild(document.createTextNode(task.value));
-    let link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    li.appendChild(link)
-    taskList.appendChild(li);
-    task.value = "";
-    e.preventDefault();
+function addTask(e) {
+    if (task.value === '') {
+        alert('Add a task')
+    } else {
+        let li = document.createElement("li");
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(task.value));
+        let link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link)
+        taskList.appendChild(li);
+        task.value = "";
+        e.preventDefault();
+    }
+
 }
